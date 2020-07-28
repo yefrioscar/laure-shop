d4038c0c5da1b973a1476be06a601ecfbcb24e34
Yefri <yefrioscar9814@gmail.com>
2020-07-28 03:26:13 -0500

    init project

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="p-4 shadow rounded bg-white">
+      <h1 className="text-purple-500 leading-normal">Next.js</h1>
+      <p className="text-gray-500">with Tailwind CSS</p>
+    </div>
+  )
+}
