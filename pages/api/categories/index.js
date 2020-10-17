6c193ed53b2180a64d5c03de6d3d15689f4f9f20
Yefri <yefrioscar9814@gmail.com>
2020-10-17 18:05:43 -0500

    update api categories

--- pages/api/categories/index.js
+++ pages/api/categories/index.js
@@ -2,24 +2,12 @@ import firebase from '../../../lib/firebase';
 
 export default async (req, res) => {
 
-    let {
-      body: {
-        name
-      }
-    } = req;
-
-
     try {
-      const collection = await firebase.collection('categories').add({
-        name: name,
-
-      })
-
       const collection = await firebase.collection('categories').where('state', '==', 'active').get();
       let data = collection.docs.map(doc => doc.data())
 
       res.statusCode = 200
-      res.json({ data: 'ok' })
+      res.json({ data })
     } catch (error) {
       console.log(error)
       res.json({ error });
