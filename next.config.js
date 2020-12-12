9c3ab54cae5563e9784815b73d46737890eff2b5
Yefri <yefrioscar9814@gmail.com>
2020-12-12 16:10:26 -0500

    update url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
    env: {
-     'NEXT_PUBLIC_API_URL': "http://ec2-204-236-240-72.compute-1.amazonaws.com:1337/graphql"
+     'NEXT_PUBLIC_API_URL': "http://ec2-204-236-240-72.compute-1.amazonaws.com:1337"
    }
 }
